8a6c840f234c7f6ef1cf1eecd0ca34bd94b90fde
Yakumo-zi <1477717283@qq.com>
2023-03-01 21:11:36 +0800

    fixed bug run.sh script

--- Tree/build/CMakeFiles/main.dir/DependInfo.cmake
+++ Tree/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -1,16 +1,15 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/yakumo/Datastructure/Tree/BinaryTree/BinaryTree.cpp" "/home/yakumo/Datastructure/Tree/build/CMakeFiles/main.dir/BinaryTree/BinaryTree.cpp.o"
-  "/home/yakumo/Datastructure/Tree/main.cpp" "/home/yakumo/Datastructure/Tree/build/CMakeFiles/main.dir/main.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yakumo/Code/datastructure/Tree/BinaryTree/BinaryTree.cpp" "CMakeFiles/main.dir/BinaryTree/BinaryTree.cpp.o" "gcc" "CMakeFiles/main.dir/BinaryTree/BinaryTree.cpp.o.d"
+  "/home/yakumo/Code/datastructure/Tree/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links.
--- Tree/build/CMakeFiles/main.dir/cmake_clean.cmake
+++ Tree/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -1,6 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/main.dir/BinaryTree/BinaryTree.cpp.o"
+  "CMakeFiles/main.dir/BinaryTree/BinaryTree.cpp.o.d"
   "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
   "main"
   "main.pdb"
 )
